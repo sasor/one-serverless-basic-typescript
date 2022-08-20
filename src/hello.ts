9fd4d145062582ca19f79cb990afbe313c864884
sasor <s.leamsi99@gmail.com>
2022-08-20 18:13:35 -0400

    init

---
+++ src/hello.ts
@@ -0,0 +1,19 @@
+import { 
+    APIGatewayProxyEvent,
+    APIGatewayProxyHandler,
+    APIGatewayProxyResult, 
+    Context 
+} from "aws-lambda"
+
+export const handler: APIGatewayProxyHandler = async (
+    event: APIGatewayProxyEvent, 
+    context: Context
+): Promise<APIGatewayProxyResult> => {
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(context)
+    }
+}
